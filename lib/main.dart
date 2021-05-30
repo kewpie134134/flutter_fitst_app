a63788b6a66bd93b94c83cef3c6ace6475eecbd1
Baba Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2021-05-30 21:05:44 +0900

    [update] StatefulWidget を使用した書き方に改良

--- lib/main.dart
+++ lib/main.dart
@@ -26,8 +26,26 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(title: Text('Welcome to Flutter')),
           // body では様々な表現が可能で、今回は Center で中央配置、Text で文字列を配置している
           body: Center(
-            child: Text(wordPair.asPascalCase),
+            child: RandomWords(),
           )),
     );
   }
+}
+
+// 最小の状態保持クラスを作成する (StatefulWidget の中身部分)
+// State<RondomWords> と書くことで、汎用の State クラスのジェネリクスに RandomWords を記載し、
+// RandomWords ウィジェットの状態を維持できるようにする
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+
+// RandomWords ウィジェットを、StatefulWidget を継承することで作成する
+// (State クラスのインスタンスを作成する、StatefulWidget クラス)
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
 }
